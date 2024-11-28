0a7c93cb8e939640601efbc55da7e2edd625b44d
jonhealy1 <jonathan.d.healy@gmail.com>
2024-11-28 12:01:15 +0800

    add nav component

--- stacchain-website/src/components/Header.js
+++ stacchain-website/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Nav from "./Nav";
 
 const Header = () => {
   return (
@@ -15,34 +16,7 @@ const Header = () => {
       </div>
 
       {/* Right: Navigation (Stacked Vertically with Icons) */}
-      <nav className="flex flex-col space-y-2">
-        <a
-          href="#home"
-          className="flex items-center text-white hover:text-gray-300"
-        >
-          <i className="fas fa-home mr-2"></i> Home
-        </a>
-        <a
-          href="#about"
-          className="flex items-center text-white hover:text-gray-300"
-        >
-          <i className="fas fa-info-circle mr-2"></i> About
-        </a>
-        <a
-          href="https://github.com/stacchain"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex items-center text-white hover:text-gray-300"
-        >
-          <i className="fab fa-github mr-2"></i> GitHub
-        </a>
-        <a
-          href="#contact"
-          className="flex items-center text-white hover:text-gray-300"
-        >
-          <i className="fas fa-envelope mr-2"></i> Contact
-        </a>
-      </nav>
+      <Nav />
     </header>
   );
 };
---
+++ stacchain-website/src/components/Nav.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav className="flex flex-col space-y-2">
+      <a
+        href="#home"
+        className="flex items-center text-white hover:text-gray-300"
+      >
+        <i className="fas fa-home mr-2"></i> Home
+      </a>
+      <a
+        href="#about"
+        className="flex items-center text-white hover:text-gray-300"
+      >
+        <i className="fas fa-info-circle mr-2"></i> About
+      </a>
+      <a
+        href="https://github.com/stacchain"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center text-white hover:text-gray-300"
+      >
+        <i className="fab fa-github mr-2"></i> GitHub
+      </a>
+      <a
+        href="#contact"
+        className="flex items-center text-white hover:text-gray-300"
+      >
+        <i className="fas fa-envelope mr-2"></i> Contact
+      </a>
+    </nav>
+  );
+};
+
+export default Nav;
