f6ef84bed91467e28db9e6b8fbae16441c98600d
jonhealy1 <jonathan.d.healy@gmail.com>
2024-11-28 13:41:48 +0800

    increase logo size

--- stacchain-website/src/components/Header.js
+++ stacchain-website/src/components/Header.js
@@ -6,14 +6,14 @@ const Header = () => {
   return (
     <header>
       {/* Gradient Background for the Logo and Navigation */}
-      <div className="bg-gradient-to-b from-blue-400 to-blue-200 w-full py-14">
+      <div className="bg-gradient-to-b from-blue-400 to-blue-100 w-full py-14">
         <div className="max-w-screen-xl mx-auto px-4 flex justify-between items-center">
           {/* Logo and Tagline */}
           <div className="flex flex-col items-start">
             <img
               src={logo}
               alt="StacChain Logo"
-              className="h-72 md:h-96" // Adjust size as needed
+              className="scale-125" // Adjust size as needed
             />
             {/* <p className="text-black text-lg mt-2">geospatial economics</p> */}
           </div>
