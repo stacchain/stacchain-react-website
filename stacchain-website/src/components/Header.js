147d8a3d796ae175cf45f37d5c7748854afaad3a
jonhealy1 <jonathan.d.healy@gmail.com>
2024-11-28 12:52:53 +0800

    use logo

--- stacchain-website/src/components/Header.js
+++ stacchain-website/src/components/Header.js
@@ -1,25 +1,21 @@
 import React from "react";
 import Nav from "./Nav";
+import logo from "../assets/stacchain-logo.svg"; // Adjust path based on your file structure
 
 const Header = () => {
   return (
     <header>
       {/* Gradient Background for the Logo and Navigation */}
-      <div className="bg-gradient-to-b from-gray-400 to-gray-300 w-full py-14">
+      <div className="bg-gradient-to-b from-blue-400 to-blue-200 w-full py-14">
         <div className="max-w-screen-xl mx-auto px-4 flex justify-between items-center">
           {/* Logo and Tagline */}
-          <div>
-            <div className="font-comfortaa text-clamp">
-              <span className="text-black">stac</span>
-              <span className="text-white">chain</span>
-            </div>
-            <p
-              className="text-black text-lg mt-1"
-              style={{ fontSize: "clamp(0.8rem, 1.1vw, 1.7rem)" }}
-            >
-              Data integrity, provenance, discovery and economics for the
-              geospatial community.
-            </p>
+          <div className="flex flex-col items-start">
+            <img
+              src={logo}
+              alt="StacChain Logo"
+              className="h-72 md:h-96" // Adjust size as needed
+            />
+            {/* <p className="text-black text-lg mt-2">geospatial economics</p> */}
           </div>
 
           {/* Navigation */}
